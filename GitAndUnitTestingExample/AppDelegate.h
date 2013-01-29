7e47de77ccee323547f978f027af8b6b34a3630f
Unbounded Solutions <unbounded@usis-MacBook-Pro.local>
2013-01-29 10:35:37 -0500

    Initial Commit

---
+++ GitAndUnitTestingExample/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  GitAndUnitTestingExample
+//
+//  Created by Unbounded Solutions on 1/29/13.
+//  Copyright (c) 2013 unbounded. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ GitAndUnitTestingExampleTests/GitAndUnitTestingExampleTests.h
@@ -0,0 +1,13 @@
+//
+//  GitAndUnitTestingExampleTests.h
+//  GitAndUnitTestingExampleTests
+//
+//  Created by Unbounded Solutions on 1/29/13.
+//  Copyright (c) 2013 unbounded. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface GitAndUnitTestingExampleTests : SenTestCase
+
+@end
